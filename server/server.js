7fc74122f3d03662d98a4c9717f02c2beebe6716
anuskamhzn <anuskamhzn33@gmail.com>
2024-06-26 13:55:45 +0545

    SnapGreet

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
 import cors from "cors";
-// import path from "path";
+import path from "path";
 // import { fileURLToPath } from 'url';
 import authRoutes from "./routes/authRoutes.js";
 import birthdayRoute from "./routes/birthdayRoute.js";
@@ -41,10 +41,10 @@ app.get("/", (req, res) => {
 // const __dirname = path.dirname(__filename);
 
 // Serve static files
-// app.use(express.static(path.join(__dirname, "../frontend/build")));
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
-// });
+app.use(express.static("../frontend/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend","build", "index.html"));
+});
 
 // PORT
 const PORT = process.env.PORT || 5000;
