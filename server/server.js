3186548b2548f525042403f7fb5f8608e486b490
your-username <your-email>
2024-06-26 18:06:02 +0545

    sujen

--- server/server.js
+++ server/server.js
@@ -37,10 +37,10 @@ app.get("/", (req, res) => {
 //PORT
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static("../frontend/build"));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-});
+// app.use(express.static("../frontend/build"));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+// });
 
 //run listen
 app.listen(PORT, () => {
